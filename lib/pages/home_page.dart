8edef4706c061b300a636a287eecf3e66afb4978
anjalstha <lostnjl@gmail.com>
2022-12-27 12:19:23 +0545

    Added Drag Drop List On HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
 
 //sign user out method
@@ -11,19 +16,48 @@ class HomePage extends StatelessWidget {
     FirebaseAuth.instance.signOut();
   }
 
+  //list of tiles
+  final List myTiles = [
+    'A',
+    'B',
+    'C',
+    'D',
+  ];
+
+// reorder method
+  void updateMyTiles(int oldIndex, int newIndex) {
+    setState(() {
+      //adjustment for moving tiles down
+      if (oldIndex < newIndex) {
+        newIndex--;
+      }
+
+      //get the tile
+      final tile = myTiles.removeAt(oldIndex);
+
+      //place the tile
+      myTiles.insert(newIndex, tile);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Center(child: const Text('Drag Drop List')),
         actions: [
           IconButton(onPressed: signUerOut, icon: const Icon(Icons.logout))
         ],
       ),
-      body: Center(
-        child: Text(
-          'Logged in as ${user.email!}',
-          style: const TextStyle(fontSize: 20),
-        ),
+      body: ReorderableListView(
+        children: [
+          for (final tile in myTiles)
+            ListTile(
+              key: ValueKey(tile),
+              title: Text(tile),
+            ),
+        ],
+        onReorder: (oldIndex, newIndex) => updateMyTiles(oldIndex, newIndex),
       ),
     );
   }
